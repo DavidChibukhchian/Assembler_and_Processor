4edb80d98b985df6c0ed5730ee61177e8a997ce7
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-10-23 20:42:28 +0300

    Create main.cpp

---
+++ CPU/source/main.cpp
@@ -0,0 +1,23 @@
+#include "Stack.h"
+#include "CPU.h"
+
+int main()
+{
+    FILE* ASM_out = fopen("C:\\Users\\david\\Desktop\\Projects CLion\\Task4\\CPU\\ASM_out.txt", "r");
+    if (ASM_out == nullptr)
+    {
+        return -1;
+    }
+
+    int* code = read_input_file(ASM_out);
+    fclose(ASM_out);
+
+    Stack stk1 = {};
+    stackCtor(&stk1, 1);
+
+    run_code(code, &stk1);
+
+    stackDisplay(&stk1);
+
+    return 0;
+}
