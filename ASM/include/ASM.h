c07703e10208dc99ac445fb69b939608c401ae36
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-10-24 00:56:19 +0300

    Add files via upload

--- ASM/include/ASM.h
+++ ASM/include/ASM.h
@@ -1,14 +1,20 @@
 #ifndef ASM_ASM_H
 #define ASM_ASM_H
 
-struct program_text
+struct text_struct
 {
-    char** text;
+    char** array_of_commands;
     size_t number_of_commands;
 };
 
-program_text read_input_file(FILE* ASM_in);
+text_struct record_commands_to_buffer(FILE* ASM_in);
 
-void record_code_to_file(FILE* ASM_out, void* Program);
+struct code_struct
+{
+    int* pointer;
+    size_t number_of_elements;
+};
+
+code_struct create_code_array(void* text_of_commands);
 
 #endif //ASM_ASM_H
