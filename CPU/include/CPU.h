8f37c23de8ebde1ec23ff09f82da6d1fc53a1fe7
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-11-16 00:28:25 +0300

    Add files via upload

--- CPU/include/CPU.h
+++ CPU/include/CPU.h
@@ -4,13 +4,12 @@
 #include "Logger.h"
 #include "Stack.h"
 
-#define ASSERT(condition, err, code) {  if (!(condition))           \
-                                        {                           \
-                                            code;                   \
-                                            dump(logfile, err);     \
-                                            fclose(logfile);        \
-                                            return err;             \
-                                        }                           }
+#define ASSERT(condition, err) {  if (!(condition))           \
+                                  {                           \
+                                      dump(logfile, err);     \
+                                      fclose(logfile);        \
+                                      return err;             \
+                                  }                           }
 
 #define VERIFY(err) {  if (err)                    \
                        {                           \
@@ -25,7 +24,11 @@ struct code_struct
     size_t size;
 };
 
+int check_file(FILE* ASM_out);
+
 char* read_code_to_buffer(FILE* ASM_out, int* err);
+
+int check_code_version(char* code);
 
 int run_code(char* code, Stack* stk, int* registers);
 
--- CPU/include/Logger.h
+++ CPU/include/Logger.h
@@ -7,17 +7,26 @@ enum Errors
     Failed_To_Create_Logfile           = 1,
     Incorrect_Number_Of_CMD_Arguments  = 2,
     Failed_To_Open_Input_File          = 3,
-    Input_File_Is_Empty                = 4,
-    Failed_To_Create_Buffer            = 5,
-    Missed_HLT_Command                 = 6,
-    Too_Few_Elements_In_Stack          = 7,
-    Entered_Invalid_Value              = 8,
-    Failed_To_Create_Dump_File         = 9,
-    Failed_To_Create_Array_Of_Commands = 10,
-    Failed_To_Create_Array_Of_Code     = 11,
-    Failed_To_Resize_Array_Of_Code     = 12,
-    Invalid_Syntax                     = 13,
-    Failed_To_Create_Output_File       = 14
+
+    Invalid_Type_Of_Input_File         = 4,
+    Outdated_Version_Of_Code           = 5,
+    Incorrect_Version_Of_Code          = 6,
+    Input_File_Is_Empty                = 7,
+
+    Failed_To_Create_Buffer            = 8,
+    Failed_To_Write_Code_To_File       = 9,
+    Missed_HLT_Command                 = 10,
+    Too_Few_Elements_In_Stack          = 11,
+
+    Division_By_Zero                   = 12,
+    Entered_Invalid_Value              = 13,
+    Failed_To_Create_Dump_File         = 14,
+    Failed_To_Create_Array_Of_Commands = 15,
+
+    Failed_To_Create_Array_Of_Code     = 16,
+    Failed_To_Resize_Array_Of_Code     = 17,
+    Invalid_Syntax                     = 18,
+    Failed_To_Create_Output_File       = 19
 };
 
 void dump(FILE* logfile, int err);
